8f90e974a8fc93ab60ccd78e2959011bc5869658
peter <petermyler100@gmail.com>
2024-02-19 11:55:46 +0000

    Revised equalsGOAL function

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-from itertools import permutations
-
-
-def tryEval(equation):
-    try:
-        return eval(equation)
-    except ZeroDivisionError:
-        return 0
-
-
+from itertools import permutations, product
 nums = [9, 6, 7, 7]
 ops = ["*", "/", "+", "-"]
 GOAL = 10
@@ -15,30 +6,37 @@ count = 0
 no_brackets = []
 brackets = []
 
+
+def equalsGOAL(equation):
+    global count, GOAL
+    try:
+        res = eval(equation)
+    except ZeroDivisionError:
+        return
+
+    if res != GOAL: return
+
+    count += 1
+    if "(" in equation: brackets.append(equation)
+    else: no_brackets.append(equation)
+
+
 nums = [*map(str, nums)]
 for perm in set(permutations(nums)):
-    for op1, op2, op3 in set(permutations(ops * 3, 3)):
-        eq = "".join([perm[0], op1, perm[1], op2, perm[2], op3, perm[3]])
-
+    for op1, op2, op3 in set([(a, b, c) for a, b, c in product(*[ops]*3)]):
         # no brackets
-        if tryEval(eq) == GOAL:
-            count += 1
-            no_brackets.append(eq)
+        eq = "".join([perm[0], op1, perm[1], op2, perm[2], op3, perm[3]])
+        equalsGOAL(eq)
 
-        # small bracket
+        # brackets for one operation
         for i in range(3):
             eq2 = eq[:i*2]+"("+eq[i*2:i*2+3]+")"+eq[i*2+3:]
-            if tryEval(eq2) == GOAL:
-                count += 1
-                brackets.append(eq2)
+            equalsGOAL(eq2)
 
-        # long bracket
+        # brackets for two operations
         for i in range(2):
             eq3 = eq[:i * 2] + "(" + eq[i * 2:i * 2 + 5] + ")" + eq[i * 2 + 5:]
-
-            if tryEval(eq3) == GOAL:
-                count += 1
-                brackets.append(eq3)
+            equalsGOAL(eq3)
 
 
 print("Solutions found:", count)
