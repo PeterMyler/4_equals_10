14aa5180fee8964aa502d28dff15c178ce187bc7
PeterTheCool <petermyler100@gmail.com>
2024-03-19 21:45:44 +0000

    add type annotations to functions

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
-from itertools import permutations, product
-ops = ["*", "/", "+", "-"]  # available mathematical operations to use
+from itertools import permutations, product, combinations
+
+ops = ["*", "/", "+", "-"]  # mathematical operations
 GOAL = 10  # evaluation goal
-count = 0  # amount of successful equations found
-solutions = [[], []]  # array of successful solutions without brackets and with brackets
+nums = [9, 6, 7, 7]  # digits to solve for
 
-nums = [9, 6, 7, 7]  # the 4 digits to solve for
-nums = list(map(str, nums))  # convert all the digits to strings
+nums = list(map(str, nums))
 digit_count = len(nums)
+no_brackets = []
+brackets = []
 
 
-def equalsGOAL(equation):
+def equalsGOAL(equation: str, has_brackets: bool) -> None:
     """Evaluates a given mathematical equation, compares it to the GOAL
     and appends it to the appropriate list."""
-    global count, solutions, GOAL
-
     try:
-        res = eval(equation)
+        if eval(equation) != GOAL: return
     except ZeroDivisionError:
         return
 
-    if res != GOAL: return
-
-    count += 1
-    solutions[int("(" in equation)].append(equation)
-    # bool is 1 if True, appending to solutions[1]
-    # bool is 0 if False, appending to solutions[0]
+    if has_brackets:
+        brackets.append(equation)
+    else:
+        no_brackets.append(equation)
 
 
-def insertBrackets(string, index1, index2):
+def insertBrackets(string: str, index1: int, index2: int) -> str:
+    """Inserts a left bracket at index1 and a right bracket at index2."""
     return string[:index1] + "(" + string[index1:index2] + ")" + string[index2:]
 
 
@@ -35,24 +33,25 @@ for perm in set(permutations(nums)):
     for op in product(ops, repeat=digit_count - 1):
         # no brackets
         eq = "".join(perm[i]+op[i] for i in range(len(op)))+perm[-1]
-        equalsGOAL(eq)
+        equalsGOAL(eq, False)
 
         # brackets for one operation
         for i in range(3):
             eq2 = insertBrackets(eq, i*2, i*2+3)
-            equalsGOAL(eq2)
+            equalsGOAL(eq2, True)
 
         # brackets for two operations
         for i in range(2):
             eq3 = insertBrackets(eq, i*2, i*2+5)
-            equalsGOAL(eq3)
+            equalsGOAL(eq3, True)
 
-print("Solutions found:", count)
+print("Solutions found:", len(brackets) + len(no_brackets))
 
 print("Without brackets:")
-for s in solutions[0]:
+for s in no_brackets or [["None"]]:
     print(*s)
 
+
 print("\nWith brackets:")
-for s in solutions[1]:
+for s in brackets or [["None"]]:
     print(*s)
